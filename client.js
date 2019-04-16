a4c39d2ddb292d0c2e984f2bc01caa1a781186d4
Ricardo Casares <ricardocasares@gmail.com>
2019-04-16 04:10:47 +0200

    feat: pagination parameters BREAKING: Query now is an  input object Pagination is now required

--- client.js
+++ client.js
@@ -1,27 +1,31 @@
+const { encode } = require("querystring");
 const fetch = require("isomorphic-unfetch");
 
 const URL = "http://www.radio-browser.info/webservice";
 const STATIONS = `/json/stations`;
-const BY_ID = `${STATIONS}/byid`;
-const BY_NAME = `${STATIONS}/byname`;
+const BY_ID = `${URL}${STATIONS}/byid`;
+const SEARCH = `${URL}${STATIONS}/search`;
 
-const cfg = {
-  method: "POST",
-  headers: {
-    Accept: "application/json",
-    "Content-Type": "application/json",
-    "User-Agent": "bemba/radio-gql"
-  },
-  body: JSON.stringify({ order: "votes", reverse: "true", limit: 50 })
-};
+const request = (url, query = {}, pagination = {}) =>
+  console.log([url, "?", encode(query)].join("")) ||
+  fetch([url, "?", encode(query)].join(""), {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+      "User-Agent": "bemba/radio-gql"
+    },
+    body: JSON.stringify({
+      order: "votes",
+      reverse: "true",
+      limit: 50,
+      ...pagination
+    })
+  }).then(res => res.json());
 
-const get = async id =>
-  fetch(`${URL}${BY_ID}/${id}`, cfg)
-    .then(res => res.json())
-    .then(([station]) => station);
-
-const search = async name =>
-  fetch(`${URL}${BY_NAME}/${name}`, cfg).then(res => res.json());
+const get = id => request(`${BY_ID}/${id}`).then(([station]) => station);
+const search = ({ query, pagination }) =>
+  request(`${SEARCH}`, query, pagination);
 
 module.exports = {
   get,
--- schema.js
+++ schema.js
@@ -4,9 +4,31 @@ const {
   GraphQLList,
   GraphQLSchema,
   GraphQLString,
+  GraphQLBoolean,
   GraphQLNonNull,
-  GraphQLObjectType
+  GraphQLEnumType,
+  GraphQLObjectType,
+  GraphQLInputObjectType
 } = require("graphql");
+
+var OrderType = new GraphQLEnumType({
+  name: "Order",
+  values: {
+    NAME: { value: "name" },
+    URL: { value: "url" },
+    TAGS: { value: "tags" },
+    COUNTRY: { value: "country" },
+    STATE: { value: "state" },
+    LANGUAGE: { value: "language" },
+    VOTES: { value: "votes" },
+    CODEC: { value: "codec" },
+    BITRATE: { value: "bitrate" },
+    LAST_CHECK_OK: { value: "lastcheckok" },
+    LAST_CHECK_TIME: { value: "lastchecktime" },
+    CLICK_TIMESTAMP: { value: "clicktimestamp" },
+    CLICK_COUNT: { value: "clickcount" }
+  }
+});
 
 const StationType = new GraphQLObjectType({
   name: "Station",
@@ -52,6 +74,29 @@ const StationType = new GraphQLObjectType({
   })
 });
 
+const QueryParams = new GraphQLInputObjectType({
+  name: "QueryParams",
+  description: "Query parameters",
+  fields: {
+    tag: { type: GraphQLString },
+    name: { type: GraphQLString },
+    country: { type: GraphQLString },
+    state: { type: GraphQLString },
+    language: { type: GraphQLString }
+  }
+});
+
+const PaginationParams = new GraphQLInputObjectType({
+  name: "PaginationParams",
+  description: "Pagination parameters",
+  fields: {
+    order: { type: OrderType },
+    limit: { type: GraphQLInt },
+    offset: { type: GraphQLInt },
+    reverse: { type: GraphQLBoolean }
+  }
+});
+
 const schema = new GraphQLSchema({
   query: new GraphQLObjectType({
     name: "Query",
@@ -62,14 +107,15 @@ const schema = new GraphQLSchema({
         args: {
           id: { type: new GraphQLNonNull(GraphQLInt) }
         },
-        resolve: async (root, { id }) => get(id)
+        resolve: (root, { id }) => get(id)
       },
       stations: {
         type: new GraphQLList(StationType),
         args: {
-          query: { type: new GraphQLNonNull(GraphQLString) }
+          query: { type: QueryParams },
+          pagination: { type: PaginationParams }
         },
-        resolve: async (root, { query }) => search(query)
+        resolve: (root, query) => search(query)
       }
     })
   })
